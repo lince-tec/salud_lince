cf2f31fb3632f3449d3d3ca143f01663394eaba9
rendonC23 <178052836+rendonC23@users.noreply.github.com>
2026-05-12 21:15:00 -0600

    Añado urls para los templates de recuperar contraseña.

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from apps.usuarios import views
 
+from django.contrib.auth import views as auth_views
+from apps.usuarios.forms import RecuperarPasswordForm
+
 urlpatterns = [
     path("", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
@@ -15,4 +18,11 @@ urlpatterns = [
     path("medico/consultas/", views.medico_consultas_view, name="medico_consultas"),
     path("medico/historiales/", views.medico_historiales_view, name="medico_historiales"),
     path( "medico/editar_historial/<str:pk>/", views.editar_historial_view, name="editar_historial", ),
+
+    path("recuperar/", auth_views.PasswordResetView.as_view(template_name="contrasena/recuperar_contrasena.html", email_template_name= "contrasena/email_recuperacion.html", form_class=RecuperarPasswordForm), name="password_reset",),
+    path("recuperar/enviado", auth_views.PasswordResetDoneView.as_view(template_name="contrasena/recuperar_enviado.html"), name="password_reset_done",),
+    path("recuperar/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="contrasena/confirmar_contrasena.html"), name="password_reset_confirm",),
+    path("recuperar/completo", auth_views.PasswordResetCompleteView.as_view(template_name="contrasena/recuperacion_completa.html"), name="password_reset_complete",),
+
+
 ]
