d1dd7fc0be0f4d5b57858204f61f98b754177c6d
rendonC23 <178052836+rendonC23@users.noreply.github.com>
2026-05-19 11:44:00 -0600

    Agrego variable para la validez del token de recuperación de contraseña.

--- sistema_medico/settings.py
+++ sistema_medico/settings.py
@@ -201,3 +201,4 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 10  # Tiempo de espera para la conexión SMTP en segundos
+PASSWORD_RESET_TIMEOUT = 3600  # Tiempo de validez del token de recuperación (1 hora)
