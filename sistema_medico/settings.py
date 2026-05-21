83fdadaf192d770b3f1d8a8a380ef3e3a63aaaa9
rendonC23 <178052836+rendonC23@users.noreply.github.com>
2026-05-21 09:53:58 -0600

    Cambio el servidor SMTP para el envio de correos.

--- sistema_medico/settings.py
+++ sistema_medico/settings.py
@@ -182,13 +182,12 @@ MESSAGE_TAGS = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "saludlince680@gmail.com"
 EMAIL_TIMEOUT = 10  # Tiempo de espera para la conexión SMTP en segundos
 PASSWORD_RESET_TIMEOUT = 3600  # Tiempo de validez del token de recuperación (1 hora)
 
