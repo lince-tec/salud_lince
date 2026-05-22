86cdebdbe8bce2ce440977e7a34065238543a272
faviolamtzdiego <faviolamtzdiego@gmail.com>
2026-05-21 20:53:05 -0600

    Corregí respuestas del chatbot

--- chatbot/services/respuestas.py
+++ chatbot/services/respuestas.py
@@ -3,7 +3,7 @@ import random
 RESPUESTAS_DATA = {
     "saludo": [
         "¡Hola! ¿Cómo puedo ayudarte hoy?",
-        "Hola, soy LincyBot 👋. ¿En qué puedo apoyarte?",
+        "Hola, soy LincyBot. ¿En qué puedo apoyarte?",
         "¡Bienvenido a Salud Lince! ¿Qué necesitas consultar?",
         "Hola, estoy aquí para guiarte. ¿Qué tienes en mente?"
     ],
@@ -15,20 +15,20 @@ RESPUESTAS_DATA = {
     ],
     "despedida": [
         "Espero haberte ayudado. ¡Cuídate mucho!",
-        "Gracias por usar Salud Lince. ¡Hasta pronto! 👋",
+        "Gracias por usar Salud Lince. ¡Hasta pronto!",
         "Si necesitas algo más, aquí estaré. ¡Lindo día!"
     ],
     "desconocido": [
-        "Lo siento, aún no sé cómo responder a eso. Solo manejo temas de Salud Lince (citas, expedientes o contraseñas).",
+        "Lo siento, aún no sé cómo responder a eso. Solo manejo temas de Salud Lince (consultas, historiales o contraseñas).",
         "No tengo información sobre eso, pero puedo ayudarte con tus datos de salud. ¿Qué prefieres hacer?",
         "Aún no estoy entrenado para responder eso. ¿Te ayudo con algo de tu cuenta o tus consultas?"
     ],
     "recuperar_password": ["En el Login busca '¿Olvidaste tu contraseña?' e ingresa tu correo institucional."],
-    "cambiar_password": ["Inicia sesión, ve a 'Mi Perfil' y en la derecha verás la opción de actualizar clave."],
+    "cambiar_password": ["Inicia sesión, ve a 'Mi Perfil' y en la derecha verás la opción de actualizar contraseña."],
     "ver_historial": ["Tu historial médico está en la pestaña 'Historial médico'."],
-    "ver_consultas": ["Consulta tus citas en el apartado 'Mis Consultas'."],
+    "ver_consultas": ["Puedes ver tus consultas en el apartado 'Mis Consultas'."],
     "agregar_contacto": ["Añade contactos de emergencia en la sección 'Mi Perfil'."],
-    "iniciar_sesion": ["Ingresa con tu matrícula/número de trabajador y tu clave institucional."],
+    "iniciar_sesion": ["Ingresa con tu matrícula/número de trabajador y tu contraseña (Si estan ingresando por primera vez, tu contraseña fue enviada a tu correo institucional)."],
     "salir": ["Haz clic en 'Salir' en la parte superior derecha."]
 }
 
